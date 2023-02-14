abbb8613f699639eb3a0b9df7f439d5c86d9df28
Doug Finney <dugfiny@gmail.com>
2023-02-14 13:32:46 -0600

    Add Products

--- Testing/IProductRepository.cs
+++ Testing/IProductRepository.cs
@@ -9,5 +9,9 @@ namespace Testing
         public Product GetProduct(int id);
 
         public void UpdateProduct(Product product);
+
+        public void InsertProduct(Product productToInsert);
+        public IEnumerable<Category> GetCategories();
+        public Product AssignCategory();
     }
 }
--- Testing/Models/Product.cs
+++ Testing/Models/Product.cs
@@ -1,4 +1,6 @@
-﻿namespace Testing.Models
+﻿using System.Collections.Generic;
+
+namespace Testing.Models
 {
     public class Product
     {
@@ -18,5 +20,7 @@
         public int OnSale { get; set; } //can also be a bool, Dapper translate
 
         public int StockLevel { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
     }
 }
